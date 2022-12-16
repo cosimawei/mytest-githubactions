bbd1688f3aa5348f62c847e923f78c5fdc4cdfe8
jiangxin <jiangxin@orientsec.com.cn>
2022-12-16 16:18:09 +0800

    feat(update): update

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: jiangxin
+ * @Date: 2022-12-16 15:52:11
+ * @Company: orientsec.com.cn
+ * @Description: 
+ */
+module.exports={
+    test(){
+        console.log('fdafsa')
+    }
+}
