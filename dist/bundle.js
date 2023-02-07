11ec22e9334194979cadc0328a7473799a4c7bbf
jiangxin <jiangxin@orientsec.com.cn>
2023-02-07 17:01:19 +0800

    测试发布2

---
+++ dist/bundle.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/*
+ * @Author: jiangxin
+ * @Date: 2022-12-16 15:52:11
+ * @Company: orientsec.com.cn
+ * @Description:
+ */
+const testfunction = () => ({
+  test() {
+    console.log("fdafsa11111");
+    console.log("fdafsa11111");
+    console.log("fdafsa11111");
+    console.log("fdafsa11111222");
+  }
+});
+
+exports.testfunction = testfunction;
